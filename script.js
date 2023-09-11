eecb4a490971bca4bdb48190c424d533cfb1c6a7
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2023-09-11 01:59:03 -0400

    Added functionality to adjust number of pixels on canvas

--- script.js
+++ script.js
@@ -4,20 +4,17 @@ const canvasWidth = canvas.offsetWidth;
 const canvasHeight = canvas.offsetHeight;
 
 function buildCanvas() {
-    let horizontalSpace = canvasWidth / 16;
-    let verticalSpace = canvasHeight / 16;
+    let num = 30;
+    let pixelSize = 512 / num;
 
-    for(let i = 0; i < horizontalSpace; i++) {
-        for(let b = 0; b < verticalSpace; b++) {
-            //Create pixels
-            let pixel = document.createElement("div");
-            pixel.style.cssText = `height: 16px; width: 16px; background-color: #e2dacf;`;
-            canvas.appendChild(pixel);
+    //Create pixels
+    for(let i = 0; i < num * num; i++) {
+        let pixel = document.createElement("div");
+        pixel.setAttribute("style", `height: ${pixelSize}px; width: ${pixelSize}px;`); 
+        pixel.style.backgroundColor = "white"
+        pixel.setAttribute("class", "pixel");
+        canvas.appendChild(pixel);
         }
     }
-}
-
-
-
 
 buildCanvas();

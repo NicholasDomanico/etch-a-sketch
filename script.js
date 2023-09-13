4c4b76c41adcf285537deb492c88f4140aa945b7
Nicholas Domanico <3966361+NicholasDomanico@users.noreply.github.com>
2023-09-13 02:16:26 -0400

    Added ability to change canvas size with the sider

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
 const canvas = document.querySelector("#canvas");
 const colorSelector = document.querySelector("#colorPicker");
 const randomColorButton = document.querySelector("#randomColor")
+const canvasSizeSlider = document.querySelector("#sizeSelector");
 
 const canvasWidth = canvas.offsetWidth;
 const canvasHeight = canvas.offsetHeight;
 
-let num = 30;
-let pixelSize = 512 / num;
+const defaultCanvasSize = 30
+let pixelSize = 512 / defaultCanvasSize;
 
 let color = "#000000";
-//let isColorRandom = randomColorButton.value;
 
-//randomColorButton.addEventListener("change", function() {
-//    if (this.checked) {
-//        isColorRandom = true;
-//        color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-//    } else {
-//        isColorRandom = false;
-//    }
-//})
-
-//colorSelector.addEventListener("input", function() {
-//    if (randomColor == false) {
-//        color = this.value;
-//    }
-//});
-
-function buildCanvas() {
+function buildCanvas(canvasSize) {
     //Create pixels
-    for(let i = 0; i < num * num; i++) {
+    for(let i = 0; i < canvasSize * canvasSize; i++) {
         let pixel = document.createElement("div");
-        pixel.setAttribute("style", `height: ${pixelSize}px; width: ${pixelSize}px;`); 
+        pixel.setAttribute("style", `height: ${512/canvasSize}px; width: ${512/canvasSize}px;`); 
         pixel.style.backgroundColor = "white"
         pixel.setAttribute("class", "pixel");
         canvas.appendChild(pixel);
         }
-}   
-buildCanvas();
 
-let pixelList = document.getElementsByClassName("pixel");
-let pixelArray = [...pixelList]
+    let pixelList = document.getElementsByClassName("pixel");
+    let pixelArray = [...pixelList];
 
-for(let i = 0; i < pixelArray.length; i++) {
-    pixelArray[i].addEventListener("click", function() {
-        if (randomColorButton.checked) {
-            color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
-        } else {
-            color = colorSelector.value;
-        }
-        pixelArray[i].style.backgroundColor = color;
-    });
-}
+    for(let i = 0; i < pixelArray.length; i++) {
+        pixelArray[i].addEventListener("click", function() {
+            if (randomColorButton.checked) {
+                color = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+            } else {
+                color = colorSelector.value;
+            }
+            pixelArray[i].style.backgroundColor = color;
+        });
+    }
+}  
+
+canvasSizeSlider.addEventListener("change", function() {
+    let num = Number(this.value);
+    
+    while (canvas.firstChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
+    buildCanvas(num);
+})
+
+buildCanvas(defaultCanvasSize);
+
+
 
